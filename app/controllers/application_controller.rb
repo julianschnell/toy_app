b1118fd086c21746460814ee3116095958736233
Julian J. Schnell <julian.schnell@gmail.com>
2015-03-01 22:52:33 +0100

    initialize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def hallo
-    render text: "Hallo Beccilein! Wie geht's dir?"
-  end
 end
